04dc322943f22cbd9819eb171d231a49094f7fab
Soh <sohojiambo@outlook.com>
2023-03-16 05:57:12 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    char Code[] = "Programming is like building a multilingual puzzle";
+    char Code[] = "\"Programming is like building a multilingual puzzle";
 
     puts(Code);
 
