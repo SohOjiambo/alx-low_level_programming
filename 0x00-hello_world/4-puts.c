3c316464b2149404a4bdd4b695b6389a83ff7af1
Soh <sohojiambo@outlook.com>
2023-03-16 14:18:57 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
-int main(void)
 
 /**
- * Initializing a variable
- * main - Entry point
+ * main -Entry point
  * Description:"Programming is like building
  * a multilingual puzzle, followed by a new line
  * Return value is 0
  */
+int main(void)
+
+/*Char - Initializing variable*/
 {
 	char Code[] = "\"Programming is like building a multilingual puzzle";
 
