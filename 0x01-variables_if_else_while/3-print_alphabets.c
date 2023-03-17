9454dbc3eda66f39dff7e03d6707ebc2973869cb
Soh <sohojiambo@outlook.com>
2023-03-17 15:59:35 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase and uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        char ch, CH;
+
+        for (ch = 'a'; ch <= 'z'; ch++)
+
+        {
+                putchar(ch);
+        }
+        for (CH = 'A'; CH <= 'Z'; CH++)
+
+        {
+                putchar(CH);
+        }
+        putchar('\n');
+        return (0);
+}
+
