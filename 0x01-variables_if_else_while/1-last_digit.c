c5f17718d84cadbf38fa7efd0dacaca1b0b97a38
Soh <sohojiambo@outlook.com>
2023-03-17 14:45:52 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /*
- * main -assigns random numbers to a variable,n.
+ * main - determines greater than 5,less than 6 or 0
  *
  * Return: Always 0 (Success)
  */
