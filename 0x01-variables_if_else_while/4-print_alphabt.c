4f38a33b66186bfcb4c3c4bd4895788d36b1bc00
Soh <sohojiambo@outlook.com>
2023-03-18 02:32:59 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,14 +10,12 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-
-		if (ch != 'e' && ch != 'q');
-
-		{
+	for(ch = 'a'; ch <= 'z'; ch++)
+	{
+		if(ch != 'e' && ch != 'q');
 			putchar(ch);
-		}
+	}
 
-	putchar('\n');
+		putchar('\n');
 	return (0);
 }
