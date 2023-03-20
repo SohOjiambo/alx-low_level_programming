77f975d746423e5643150d425a44232940529207
Soh <sohojiambo@outlook.com>
2023-03-20 05:56:42 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 98; i++)
+	for (i = 0; i <= 98; i++)
 	{
-		for (j = i + 1; j < 99; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
 
 			putchar((i / 10) + '0');
@@ -22,10 +22,10 @@ int main(void)
 
 			if (i == 98 && j == 99)
 				continue;
-		}
 
-		putchar(',');
-		putchar(' ');
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 
