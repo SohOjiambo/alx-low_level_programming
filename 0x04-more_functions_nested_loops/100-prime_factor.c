436e81af620f92ad279b881159e62b6d4d1975b9
Soh <sohojiambo@outlook.com>
2023-03-23 18:06:30 -0700

    final batch

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints largest prime factor.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int n, x;
+
+	n = 612852475143;
+	for (x = 2; x <= n; x++)
+	{
+		if (n % fp == 0)
+		{
+			n /= x;
+			x--;
+		}
+	}
+	printf("%ld\n", x);
+	return (0);
+}
