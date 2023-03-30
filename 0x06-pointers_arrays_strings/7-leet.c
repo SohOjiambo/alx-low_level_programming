283785f1cfc8df9f2dd946356a7cc98729457b7e
Soh <sohojiambo@outlook.com>
2023-03-30 06:53:54 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,16 +13,26 @@ char *leet(char *x)
 
 	for (i = 0; x[i] != '\0'; i++)
 	{
-		while (x[i] == 'a' || x[i] == 'A')
+		if (x[i] == 'a' || x[i] == 'A')
+		{
 			x[i] = '4';
-		while (x[i] == 'e' || x[i] == 'E')
+		}
+		if (x[i] == 'e' || x[i] == 'E')
+		{
 			x[i] = '3';
-		while (x[i] == 'o' || x[i] == 'O')
+		}
+		if (x[i] == 'o' || x[i] == 'O')
+		{
 			x[i] = '0';
-		while (x[i] == 't' || x[i] == 'T')
+		}
+		if (x[i] == 't' || x[i] == 'T')
+		{
 			x[i] = '7';
-		while (x[i] == 'l' || x[i] == 'L')
+		}
+		if (x[i] == 'l' || x[i] == 'L')
+		{
 			x[i] = '1';
+		}
 	}
 
 	return (x);
