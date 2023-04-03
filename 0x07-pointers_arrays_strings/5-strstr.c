327322f2bcceb8e84c212d3b37761bf4cdedc1cf
Soh <sohojiambo@outlook.com>
2023-04-03 16:37:50 -0700

    fixes

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,20 +11,20 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	char h;
-	char n;
+	char *h;
+	char *n;
 
 	while (*haystack != '\0')
 	{
 		h = haystack;
 		n = needle;
 
-		while (*haystack != '\0' && *n != '\0' && haystack == *n)
+		while (*haystack != '\0' && *n != '\0' && *haystack == *n)
 		{
 			haystack++;
 			n++;
 		}
-		if (!*n)
+		if (!*n == '\0' )
 			return (h);
 		haystack = h + 1;
 	}
