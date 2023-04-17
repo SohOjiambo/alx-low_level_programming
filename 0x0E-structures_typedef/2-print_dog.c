a99a7fca96233ef4f05801f566f81ae99ea483e6
Soh <sohojiambo@outlook.com>
2023-04-17 11:13:01 -0700

    tasks

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,49 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - prints a struct dog
+ * @d: pointer to struct dog
+ *
+ * Return: no return
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	char *name = d->name
+
+	if (name == NULL)
+	}
+		name = "(nil)"
+	}
+
+	float *age = d->age;
+
+	if (age == NULL)
+	{
+		printf("Name: %s\n", name);
+		printf("Age: (nil)\n", *age);
+	}
+	else
+	{
+		printf("Name: %s\n", name);
+		printf("Age: %f\n", *age);
+	}
+
+	char *ownwe = d->owner;
+
+	if (owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("owner: %s\n", owner);
+	}
+}
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,56 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return: struct dog on success
+ * Null on failure
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *pup;
+	int x, y, z;
+	char *nm, *ow;
+
+	pup = malloc(sizeof(struct dog));
+	if (pup == null)
+		return (NULL);
+
+	for (x = 0; name[x] != '\0'; x++)
+		;
+	for (y = 0; owner[y] != '\0'; y++)
+		;
+
+
+	nm = malloc(sizeof(char) * x + 1)
+	if (nm == NULL)
+	{
+		free(pup);
+		return (NULL);
+	}
+
+	ow = malloc(sizeof(char) * y + 1)
+	if (ow == NULL)
+	{
+		free(nm);
+		free(pup);
+		return (NULL);
+	}
+
+	for (z = 0; z <= x; z++)
+		nm[z] = name[z];
+
+	for (z = 0; z <= y; z++)
+		ow[z] = owner[z];
+
+	pup->name = nm;
+	pup->age = age;
+	pup->owner = ow;
+
+	return (pup);
+}
