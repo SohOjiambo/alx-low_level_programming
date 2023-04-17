e8b509b78bb2d58e9223136ab6a52c6b3ba1795d
Soh <sohojiambo@outlook.com>
2023-04-17 10:43:40 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - structure definition of a dpg
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's age
+ *
+ * Return: no return
+ */
+
+typedef struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
