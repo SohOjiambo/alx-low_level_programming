d0421425161c6e9efdf2f8191c41e081cb5a1822
Soh <sohojiambo@outlook.com>
2023-04-20 07:43:24 -0700

    fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,61 +1,4 @@
 #include "variadic_functions.h"
-#include <stdio.h>
-#include <stdarg.h>
-
-/**
- * print_char - prints a char
- * @list: argument list
- */
-void print_char(va_list list)
-{
-	char c;
-
-	c = va_arg(list, int);
-	printf("%c", c);
-}
-
-/**
- * print_integer - prints an integer
- * @list: argument list
- */
-void print_integer(va_list list)
-{
-	int i;
-
-	i = va_arg(list, int);
-	printf("%d", i);
-}
-
-/**
- * print_float - prints a float
- * @list: argument list
- */
-void print_float(va_list list)
-{
-	float f;
-
-	f = va_arg(list, double);
-	printf("%f", f);
-}
-
-/**
- * print_string - prints a string
- * @list: argument list
- */
-void print_string(va_list list)
-{
-	char *s;
-
-	s = va_arg(list, char *);
-	if (s == NULL)
-	{
-		printf("(nil)");
-	}
-	else
-	{
-		printf("%s", s);
-	}
-}
 
 /**
  * print_all - prints anything
@@ -63,35 +6,45 @@ void print_string(va_list list)
  *
  * Return: No return
  */
+
 void print_all(const char * const format, ...)
 {
+	int i;
+	char c, *s;
+	float f;
+
 	va_list list;
-	int i = 0, j = 0;
-	char *separator = "";
-
-	print_fn_t print_fn[] = {
-		{'c', print_char},
-		{'i', print_integer},
-		{'f', print_float},
-		{'s', print_string},
-		{'\0', NULL}
-	};
-
 	va_start(list, format);
 
 	while (format && format[i])
 	{
-		j = 0;
-		while (print_fn[j].type)
+		switch (format[i])
 		{
-			if (format[i] == print_fn[j].type)
-			{
-				printf("%s", separator);
-				print_fn[j].func(list);
-				separator = ", ";
+			case 'c':
+				c = va_arg(list, int);
+				printf("%c", c);
 				break;
-			}
-			j++;
+			case 'i':
+				i = va_arg(list, int);
+				printf("%d", i);
+				break;
+			case 'f':
+				f = va_arg(list, double);
+				printf("%f", f);
+				break;
+			case 's':
+				s = va_arg(list, char *);
+				if (s == NULL)
+				{
+					printf("(nil)");
+				}
+				else
+				{
+					printf("%s", s);
+				}
+				break;
+			default:
+				break;
 		}
 		i++;
 	}
