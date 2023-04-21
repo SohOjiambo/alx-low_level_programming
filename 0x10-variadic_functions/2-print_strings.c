7a8702657d97164a261007ee90771401e17da1ec
Soh <sohojiambo@outlook.com>
2023-04-20 22:05:38 -0700

    fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,9 +14,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *str;
 
-	if (separator == NULL)
-		return;
-
 	va_start(list, n);
 
 	for (i = 0; i < n; i++)
